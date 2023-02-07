16f0803b8caa2e92888b5ff3b2cff911af5c0531
shivaji-dgraph <shivaji@dgraph.io>
2023-02-07 12:46:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [data, setData] = useState("");
+
+  const fetchData = () => {
+    return fetch("https://localhost:3000/")
+          .then((response) => response.json())
+          .then((data) => setData(data));
+  }
+console.log(data)
+  useEffect(() => {
+    fetchData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
     </div>
   );
 }
